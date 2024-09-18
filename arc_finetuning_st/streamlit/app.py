b35b54bef7bc5af6cfe4beb6295bb955f575f452
Andrei Fajardo <andrei@nerdai.io>
2024-09-17 22:35:33 -0400

    outsource graph creation to Controller

--- arc_finetuning_st/streamlit/app.py
+++ arc_finetuning_st/streamlit/app.py
@@ -19,9 +19,8 @@ def startup() -> Tuple[Controller,]:
 
 logo = '[<img src="https://d3ddy8balm3goa.cloudfront.net/llamaindex/LlamaLogoSquare.png" width="28" height="28" />](https://github.com/run-llama/llama-agents "Check out the llama-agents Github repo!")'
 st.title("ARC Task Solver Workflow with Human Input")
-st.markdown(f"_Powered by LlamaIndex_ &nbsp; {logo}", unsafe_allow_html=True)
 
-with st.container():
+with st.sidebar:
     task_selection = st.selectbox(
         label="Task",
         options=sample_tasks.keys(),
@@ -38,31 +37,21 @@ with train_col:
     with st.container(border=True, height=800):
         left, right = st.columns([1, 1], vertical_alignment="top", gap="medium")
         with left:
-            st.subheader("Input")
             selected_task = st.session_state.selected_task
             task = sample_tasks.get(selected_task, None)
             if task:
                 for ix, ex in enumerate(task["train"]):
                     grid = ex["input"]
-                    fig = px.imshow(grid, text_auto=True)
-                    fig.update_coloraxes(showscale=False)
-                    fig.update_layout(
-                        yaxis={"visible": False}, xaxis={"visible": False}
-                    )
+                    fig = Controller.plot_grid(grid, kind="input")
                     st.plotly_chart(fig, use_container_width=True)
 
         with right:
-            st.subheader("Output")
             selected_task = st.session_state.selected_task
             task = sample_tasks.get(selected_task, None)
             if task:
                 for ex in task["train"]:
                     grid = ex["output"]
-                    fig = px.imshow(grid, text_auto=True)
-                    fig.update_coloraxes(showscale=False)
-                    fig.update_layout(
-                        yaxis={"visible": False}, xaxis={"visible": False}
-                    )
+                    fig = Controller.plot_grid(grid, kind="output")
                     st.plotly_chart(fig, use_container_width=True)
 
 with test_col:
@@ -70,18 +59,14 @@ with test_col:
     with st.container(border=True):
         left, right = st.columns([1, 1], vertical_alignment="top", gap="medium")
         with left:
-            st.subheader("Input")
             selected_task = st.session_state.selected_task
             task = sample_tasks.get(selected_task, None)
             if task:
                 grid = task["test"][0]["input"]
-                fig = px.imshow(grid, text_auto=True)
-                fig.update_coloraxes(showscale=False)
-                fig.update_layout(yaxis={"visible": False}, xaxis={"visible": False})
+                fig = Controller.plot_grid(grid, kind="input")
                 st.plotly_chart(fig, use_container_width=True)
 
         with right:
-            st.subheader("Output Prediction")
             _ = st.container(border=True, height=300)
         st.text_area(label="Critique", placeholder="Enter critique.")
         st.button("predict", type="primary")
--- arc_finetuning_st/streamlit/controller.py
+++ arc_finetuning_st/streamlit/controller.py
@@ -1,6 +1,7 @@
 import logging
 import streamlit as st
-from typing import Optional
+import plotly.express as px
+from typing import Any, Optional, List, Literal
 
 from llama_deploy.control_plane import ControlPlaneConfig
 
@@ -17,3 +18,25 @@ class Controller:
         """Handle selection of ARC task."""
         selected_task = st.session_state.get("selected_task")
         print(selected_task)
+
+    @staticmethod
+    def plot_grid(
+        grid: List[List[int]], kind=Literal["input", "output", "prediction"]
+    ) -> Any:
+        m = len(grid)
+        n = len(grid[0])
+        fig = px.imshow(
+            grid, text_auto=True, labels={"x": f"{kind.title()}<br><sup>{m}x{n}</sup>"}
+        )
+        fig.update_coloraxes(showscale=False)
+        fig.update_layout(
+            yaxis={"visible": False},
+            xaxis={"visible": True, "showticklabels": False},
+            margin=dict(
+                l=20,
+                r=20,
+                b=20,
+                t=20,
+            ),
+        )
+        return fig
